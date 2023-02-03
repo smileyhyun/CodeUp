03b96d337d0c6b32fae57db3b3f6f688780f5970
smileyhyun <smileyhyunn@gmail.com>
2023-02-03 19:24:57 +0900

    --

--- 6018.py
+++ 6018.py
@@ -2,3 +2,5 @@ print("---#6018--")
 # input().split(':') 를 사용하면 콜론 ':' 기호를 기준으로 자른다.
 # print(?, ?, sep=':') 를 사용하면 콜론 ':' 기호를 사이에 두고 값을 출력한다.
 # sep 는 분류기호(seperator)를 의미한다.
+a,b=input().split(':')
+print(a,b,sep=':')
